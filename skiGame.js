0869a32206bf1be63b542f81938df831637e85dc
MikeNotInTheFlesh <mmgrace@gmail.com>
2018-06-06 11:19:06 -0700

    Add files via upload
    95% screen size

--- skiGame.js
+++ skiGame.js
@@ -41,7 +41,7 @@ function preload(){
 }
 
 function setup() {
-	createCanvas(window.innerWidth, window.innerHeight);
+	createCanvas(window.innerWidth * 0.95, window.innerHeight * 0.95);
   // createCanvas(windowWidth * 3 / 4, windowHeight * 3 / 4);
 //  createCanvas(600, 400);
   frameRate(30);
